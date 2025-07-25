aa73ec85518235fadea082901733cf5437c9e1ab
Yoqub <mardonovyoqubjon50@gmail.com>
2025-07-25 01:13:09 -0700

    Done

--- config/settings.py
+++ config/settings.py
@@ -30,11 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "fallback-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = [
-    "127.0.0.1",
-    "localhost",
-    "drf-olcha-uz.up.railway.app"
-]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
